77a045b5565007e867236489c906c3d8637a27c0
John Cornell <john.cornell@smokeball.com>
2024-11-25 17:23:00 +1100

    Fixed Anthropic Max Tokens

--- ExampleTurtle/Program.cs
+++ ExampleTurtle/Program.cs
@@ -19,17 +19,19 @@ options.GetSection<JsonEngineConfigSection>()!.JsonFormat = false;
 
 //var engineModelId = new EngineModelId(EngineType.OpenAI, OpenAIModelIds.GPT4o);
 //var engineModelId = new EngineModelId(EngineType.Ollama, OllamaModelIds.Phi3);
-//var engineModelId = new EngineModelId(EngineType.Anthropic, AnthropicModelIds.Claude3_Haiku_20240307);
-var engineModelId = new EngineModelId(EngineType.AzureOpenAI, AzureOpenAIModelIds.GPT4o);
+var engineModelId = new EngineModelId(EngineType.Anthropic, AnthropicModelIds.Claude3_5_Sonnet_Latest);
+//var engineModelId = new EngineModelId(EngineType.AzureOpenAI, AzureOpenAIModelIds.GPT4o);
 //options is optional
 
 //Will initialize IConfiguration from appsettings.json, but IConfiguration can be passed directly as a named parameter
 IEngine engine = EngineFactory.Start(engineModelId, options);
 engine.SetSystemPrompt("Be sure to ALWAYS WRITE IN lyrical prose");
 
+Console.WriteLine("LLM TEST");
 var response = await engine.CallAsync("What is the capital of France?");
 Console.WriteLine(response);
 
+Console.WriteLine("STREAM TEST");
 //StreamAsync example
 await foreach (var streamresponse in engine.StreamAsync("Write a highly detailed and verbose essay on belly button fluff"))
 {

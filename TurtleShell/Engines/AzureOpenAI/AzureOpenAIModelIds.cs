d9d710f00a6a292886d857b8df76556f75654f70
John Cornell <john.cornell@smokeball.com>
2025-02-04 10:19:43 +1100

    Added 01 and 03 models

--- TurtleShell/Engines/AzureOpenAI/AzureOpenAIModelIds.cs
+++ TurtleShell/Engines/AzureOpenAI/AzureOpenAIModelIds.cs
@@ -12,5 +12,8 @@ namespace TurtleShell.Engines.AzureOpenAI
         public const string GPT4 = "gpt-4";
         public const string GPT4o = "gpt-4o";
         public const string GPT4o20240806 = "gpt-4o-2024-08-06";
+        public const string GPT03_Mini = "o3-mini";
+        public const string GPT01 = "o1";
+        public const string GPT01_Mini = "o1-mini";
     }
 }
